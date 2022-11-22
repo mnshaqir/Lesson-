f75c4d5187a54b843525c26d2cc334d9fefcd5c2
mnshaqir <116859685+mnshaqir@users.noreply.github.com>
2022-11-22 14:30:28 +0800

    Cuboid

---
+++ Cuboid.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuboid2
+{
+    internal class Cuboid : Shape3DCalculator
+
+    {
+
+        private double _height; //data
+        private double _length; //data
+        private double _width;  //data
+
+        public double height //propertie
+        {
+            set { _height = value; }
+            get { return _height; }
+        }
+        public double length
+        {
+            set { _length = value; }
+            get { return _length; }
+        }
+        public double width
+        {
+            set { _width = value; }
+            get { return _width; }
+        }
+
+        //Default constructor gives all zero
+
+        public Cuboid()
+        {
+            height = 0;
+            length = 0;
+            width = 0;
+        }
+
+        //Constructor that gives each value
+        public Cuboid(double _height, double _length, double _width)
+        {
+            height = _height;
+            length = _length;
+            width = _width;
+
+        }
+
+        public void ShowAllProperties()
+        {
+            Console.WriteLine("Height = {0}, Length = {1}, Width = {2}", height, length, width);
+        }
+
+        public double Volume()
+        {
+            return _height * _width * _length;
+        }
+        public double SurfaceArea()
+        {
+            return 2 * (_height * _width + _height * _length + _width * _length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuboid2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuboid aCuboid = new Cuboid(2, 3, 4);
+
+            aCuboid.ShowAllProperties();
+
+            Console.WriteLine("New parameter Height = {0}, Length = {1}, Width = {2}", aCuboid.height, aCuboid.length, aCuboid.width);
+            Console.WriteLine("Volume of Cuboid = {0}", aCuboid.Volume());
+            Console.WriteLine("Surface Area of Cuboid = {0}", aCuboid.SurfaceArea());
+            Console.ReadKey();
+
+        }
+    }
+
+    interface Shape3DCalculator
+    {
+        double Volume();
+        double SurfaceArea();
+
+    }
+
+   
+
+}
+
+
